97f0f226ab1b714b3225bff4fd0afe07e9045917
Alexandr Lyamburtsev <48863296+Nobahu@users.noreply.github.com>
2025-12-18 15:24:56 +0700

    Update entrypoint.sh

--- Lab4/worker/entrypoint.sh
+++ Lab4/worker/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-echo "Starting Hash Service Worker..."
-
-sleep 10
-
-echo "Starting Flask API..."
+echo "Starting Flask API"
 gunicorn -w 2 -b 0.0.0.0:5000 app:app --access-logfile - --error-logfile - &
-
 sleep 3
-
-echo "Starting Kafka Consumer..."
+echo "Starting Kafka Consumer"
 python worker.py &
